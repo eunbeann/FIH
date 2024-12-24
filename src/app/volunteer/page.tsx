50c5f25c25e7dc8086296e436b81bb9347c5f7aa
eunbeann <ds5bean23@gmail.com>
2024-12-23 22:21:24 -0800

    feat: add empty page

---
+++ src/app/volunteer/page.tsx
@@ -0,0 +1,10 @@
+import ExperienceBox from './components/ExperienceBox';
+
+export default function page() {
+  return (
+    <div className="flex flex-col">
+      <div>Volunteer experience</div>
+      <ExperienceBox />
+    </div>
+  );
+}
