d1a51e7e7bfe55bfaec843623bb74930b5b3d07b
eunbeann <ds5bean23@gmail.com>
2024-12-23 22:20:21 -0800

    design: setting the custom colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,51 +16,19 @@ export default {
         pretendard: ['Pretendard-Regular', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        card: {
-          DEFAULT: 'hsl(var(--card))',
-          foreground: 'hsl(var(--card-foreground))',
-        },
-        popover: {
-          DEFAULT: 'hsl(var(--popover))',
-          foreground: 'hsl(var(--popover-foreground))',
-        },
-        primary: {
-          DEFAULT: 'hsl(var(--primary))',
-          foreground: 'hsl(var(--primary-foreground))',
-        },
-        secondary: {
-          DEFAULT: 'hsl(var(--secondary))',
-          foreground: 'hsl(var(--secondary-foreground))',
-        },
-        muted: {
-          DEFAULT: 'hsl(var(--muted))',
-          foreground: 'hsl(var(--muted-foreground))',
-        },
-        accent: {
-          DEFAULT: 'hsl(var(--accent))',
-          foreground: 'hsl(var(--accent-foreground))',
-        },
-        destructive: {
-          DEFAULT: 'hsl(var(--destructive))',
-          foreground: 'hsl(var(--destructive-foreground))',
-        },
-        border: 'hsl(var(--border))',
-        input: 'hsl(var(--input))',
-        ring: 'hsl(var(--ring))',
-        chart: {
-          '1': 'hsl(var(--chart-1))',
-          '2': 'hsl(var(--chart-2))',
-          '3': 'hsl(var(--chart-3))',
-          '4': 'hsl(var(--chart-4))',
-          '5': 'hsl(var(--chart-5))',
-        },
+        white: '#ffffff',
+        gray1: '#f8f8f8',
+        gray1_3: '#e7e7e7',
+        gray1_5: '#d4e1e7',
+        gray2: '#c9d4da',
+        gray3: '#b3bac0',
+        gray4: '#979aa0',
+        gray5: '#454545',
+        gray6: '#191919',
+        black: '#000000',
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
+      borderColor: {
+        DEFAULT: 'hsl(var(--border))',
       },
     },
   },
