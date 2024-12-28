6ba1ab38c5bcb744f67ee36574c6681c8a0cff25
eunbeann <ds5bean23@gmail.com>
2024-12-27 23:05:58 -0800

    style: enforce single quotes via prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,6 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 export default {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@shadcn/ui/**/*.{js,ts,jsx,tsx}',
@@ -17,12 +16,12 @@ export default {
       },
       colors: {
         white: '#ffffff',
-        gray1: '#f8f8f8',
+        gray1: '#1C1E22',
         gray1_3: '#e7e7e7',
         gray1_5: '#d4e1e7',
         gray2: '#c9d4da',
         gray3: '#b3bac0',
-        gray4: '#979aa0',
+        gray4: '#ACB3C0',
         gray5: '#454545',
         gray6: '#191919',
         black: '#000000',
