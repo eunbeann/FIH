ef9a53f3636563b008213b6e5476a94dc1d92c04
eunbeann <ds5bean23@gmail.com>
2024-12-28 12:37:37 -0800

    feat: enhance Volunteer page layout and ExperienceBox component

--- src/app/volunteer/components/ExperienceBox.tsx
+++ src/app/volunteer/components/ExperienceBox.tsx
@@ -1,3 +1,17 @@
+import psyDuck from '@/assets/images/psyduck.jpeg';
+import Image from 'next/image';
+
 export default function ExperienceBox() {
-  return <div>ExperienceBox</div>;
+  return (
+    <div className="flex flex-col items-center">
+      <p className="pb-1 text-[12px]">Naver 1784 School</p>
+      <Image
+        src={psyDuck}
+        alt="psyDuck"
+        className="h-[12rem] w-[16rem] rounded-md bg-red-600 object-fill"
+      />
+
+      {/* <Image alt="mockImage" src="https://picsum.photos/200/300" fill /> */}
+    </div>
+  );
 }
--- src/app/volunteer/page.tsx
+++ src/app/volunteer/page.tsx
@@ -2,9 +2,18 @@ import ExperienceBox from './components/ExperienceBox';
 
 export default function page() {
   return (
-    <div className="flex flex-col">
-      <div>Volunteer experience</div>
-      <ExperienceBox />
+    <div>
+      <h1 className="mb-[3rem] text-[1.625rem] font-bold text-[#1C1E22]">
+        Volunteer Experience
+      </h1>
+      <div className="flex flex-wrap gap-[1.8rem]">
+        <ExperienceBox />
+        <ExperienceBox />
+        <ExperienceBox />
+        <ExperienceBox />
+        <ExperienceBox />
+        <ExperienceBox />
+      </div>
     </div>
   );
 }
