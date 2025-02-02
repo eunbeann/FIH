77a6947da8b31572f1a5500c859774c48d98db63
eunbeann <ds5bean23@gmail.com>
2025-02-02 15:13:34 -0800

    feat: main page replacement

---
+++ src/app/home/page.tsx
@@ -0,0 +1,32 @@
+import eunbean from '@/assets/images/eunbean.jpeg';
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="flex h-full w-full flex-col items-center xl:mx-10 xl:mt-0 xl:flex-row xl:justify-center">
+      <div className="flex h-[calc(100%-10rem)] flex-col xl:w-[40%] xl:justify-center">
+        <h1 className="pb-[20px] pt-[20px] text-[50px] font-bold leading-[50px] -tracking-[5px] xl:py-[30px] xl:text-[80px] xl:leading-[80px]">
+          Hi <br /> This is Eunbean
+        </h1>
+        <h5>
+          I am a frontend developer who enjoys sharing and empathizing with
+          others about new experiences. I strive tirelessly to develop clean and
+          readable code conducive to collaboration, and to collaborate on
+          creating web pages that everyone can empathize with.
+        </h5>
+      </div>
+
+      <div className="relative mt-[40px] flex w-[50%] max-w-[500px] items-center justify-center pb-4 xl:mt-[100px]">
+        <div className="absolute ml-10 mt-10 h-[11.8rem] w-[9.3rem] rounded-md bg-[#9FA4B0] xl:ml-16 xl:mt-16 xl:h-[20rem] xl:w-[16rem]" />
+        <Image
+          src={eunbean}
+          alt="eunbeanProfilePic"
+          className="relative z-40 h-[11.8rem] w-[9.3rem] rounded-md object-cover xl:h-[20rem] xl:w-[16rem]"
+        />
+        <div className="absolute z-50 -ml-[7rem] -mt-[12rem] flex shrink-0 -rotate-[23deg] items-center justify-center bg-gray-700 px-[1rem] py-[0.3rem] text-[1rem] text-[#DCE5EC] xl:-ml-[10rem] xl:-mt-[18rem] xl:px-[3rem]">
+          # OPEN TO WORK
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,25 @@
-import eunbean from '@/assets/images/eunbean.jpeg';
-import Image from 'next/image';
+import ContactPage from './contact/page';
+import Home from './home/page';
+import ProjectPage from './projects/page';
+import VolunteerPage from './volunteer/page';
 
-export default function Home() {
+export default function Main() {
   return (
-    <div className="flex h-full w-full flex-col items-center justify-center xl:mx-10 xl:flex-row">
-      <div className="flex h-full w-[50%] flex-col justify-center">
-        <h1 className="py-[30px] text-[80px] leading-[80px] -tracking-[5px]">
-          Hi <br /> This is Eunbean
-        </h1>
-        <h5>
-          I am a frontend developer who enjoys sharing and empathizing with
-          others about new experiences. I strive tirelessly to develop clean and
-          readable code conducive to collaboration, and to collaborate on
-          creating web pages that everyone can empathize with.
-        </h5>
+    <div className="scrollbar-hidden h-full overflow-auto xl:h-[calc(100vh-10.5rem)] xl:snap-y xl:snap-mandatory">
+      <div className="h-full xl:flex xl:snap-start xl:items-center xl:justify-center">
+        <Home />
       </div>
 
-      <div className="relative mt-[100px] flex w-[50%] max-w-[500px] items-center justify-center pb-4">
-        <div className="absolute ml-16 mt-16 h-[20rem] w-[16rem] rounded-md bg-[#9FA4B0]" />
-        <Image
-          src={eunbean}
-          alt="eunbeanProfilePic"
-          className="relative z-40 h-[20rem] w-[16rem] rounded-md object-cover"
-        />
-        <div className="absolute z-50 -ml-[10rem] -mt-[18rem] flex shrink-0 -rotate-[23deg] items-center justify-center bg-gray-700 px-[3rem] py-[0.3rem] text-[1rem] text-[#DCE5EC]">
-          # OPEN TO WORK
-        </div>
+      <div className="flex h-screen items-center justify-center xl:snap-start">
+        <ProjectPage />
+      </div>
+
+      <div className="flex h-screen items-center justify-center xl:snap-start">
+        <VolunteerPage />
+      </div>
+
+      <div className="flex h-screen items-center justify-center xl:snap-start">
+        <ContactPage />
       </div>
     </div>
   );
