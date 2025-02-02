a9b69df3ab488b2f648b802fb5f3e36a059b9c9e
eunbeann <ds5bean23@gmail.com>
2025-02-02 15:14:14 -0800

    response: response modify ProjectCard

--- src/app/projects/components/ProjectCard.tsx
+++ src/app/projects/components/ProjectCard.tsx
@@ -3,7 +3,7 @@ import dragHandle from '@/assets/icons/dragHandle.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 
-interface ProjectCardProps {
+export interface ProjectCardProps {
   title: string;
   description: string;
   tags: string;
@@ -18,20 +18,20 @@ export default function ProjectCard({
 }: ProjectCardProps) {
   return (
     <Link href={link} target="_blank" rel="noopener noreferrer">
-      <div className="h-[9rem] max-w-[33rem] rounded-[0.5rem] border-[1px] border-gray4 bg-[#F2F6FC] px-[1.6rem] py-[1rem]">
+      <div className="h-[9rem] rounded-[0.5rem] border-[1px] border-gray4 bg-[#F2F6FC] px-[1.6rem] py-[1rem] xl:max-w-[33rem]">
         <div className="mb-[0.6rem] flex justify-between gap-[0.5rem] font-bold">
           <div className="flex items-center gap-[0.5rem] font-bold">
             <Image src={bookMark} alt={bookMark} />
-            <p className="font-bold text-[#0F54C2]">{title}</p>
+            <p className="text-[18px] font-bold text-[#0F54C2]">{title}</p>
           </div>
           <Image src={dragHandle} alt={dragHandle} />
         </div>
-        <p className="mb-[0.6rem] h-[3rem] whitespace-break-spaces">
+        <p className="mb-[0.6rem] h-[3rem] whitespace-break-spaces text-[14.5px] leading-[17px]">
           {description}
         </p>
         <div className="flex items-center gap-[0.2rem]">
           <div className="h-[0.8rem] w-[0.8rem] rounded-full border-[#316DAF] bg-[#3078C6]" />
-          <p>{tags}</p>
+          <p className="text-[13px]">{tags}</p>
         </div>
       </div>
     </Link>
