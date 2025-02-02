82127e6a88573e825f561e6d3457a898e01506e6
eunbeann <ds5bean23@gmail.com>
2025-02-02 10:39:35 -0800

    design: detail edit

--- src/app/volunteer/components/ExperienceBox.tsx
+++ src/app/volunteer/components/ExperienceBox.tsx
@@ -7,12 +7,14 @@ interface ExperienceBoxProps {
 
 export default function ExperienceBox({ title, src }: ExperienceBoxProps) {
   return (
-    <div className="flex flex-col items-center">
-      <p className="pb-1 text-[16px]">{title}</p>
+    <div className="flex flex-col items-center justify-center">
+      <p className="flex h-12 items-center justify-center pb-1 text-center text-[16px]">
+        {title}
+      </p>
       <Image
         src={src}
         alt="psyDuck"
-        className="h-[12rem] w-full rounded-md object-fill"
+        className="h-[12rem] w-[315px] rounded-md object-fill"
       />
     </div>
   );
