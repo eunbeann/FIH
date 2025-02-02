5ca037a05f23bcf73858f5faf04a61d1f39f9016
eunbeann <ds5bean23@gmail.com>
2025-02-02 10:39:53 -0800

    feat: add real pictures

--- src/app/volunteer/page.tsx
+++ src/app/volunteer/page.tsx
@@ -8,9 +8,20 @@ import likelion from '@/assets/images/volunteer/likelion.png';
 import naver from '@/assets/images/volunteer/naver1784.png';
 import yeogithon from '@/assets/images/volunteer/yeogithon.png';
 
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 import ExperienceBox from './components/ExperienceBox';
 
 const volunteerList = [
+  {
+    title: 'LikeLion',
+    src: likelion,
+  },
   {
     title: 'BEOTKKOTTHON',
     src: bbeot1,
@@ -36,10 +47,6 @@ const volunteerList = [
     src: jeju,
   },
   {
-    title: 'LikeLion',
-    src: likelion,
-  },
-  {
     title: 'Naver 1784 School',
     src: naver,
   },
@@ -49,16 +56,45 @@ const volunteerList = [
   },
 ];
 
-export default function page() {
+export default function VolunteerPage() {
+  const groupItems = <T,>(items: T[], groupSize: number): T[][] => {
+    const groups = [];
+    for (let i = 0; i < items.length; i += groupSize) {
+      groups.push(items.slice(i, i + groupSize));
+    }
+    return groups;
+  };
+
+  console.log(groupItems(volunteerList, 6));
+
   return (
-    <div className="w-full">
+    <div className="h-full w-full">
       <h1 className="mb-[3rem] text-[1.625rem] font-bold text-[#1C1E22]">
         Volunteer Experience
       </h1>
-      <div className="my-[3rem] grid grid-cols-3 grid-rows-2 gap-[1.8rem]">
-        {volunteerList.map((item, index) => (
-          <ExperienceBox key={index} title={item.title} src={item.src} />
-        ))}
+      <div className="my-[3rem] gap-[1.8rem]">
+        <Carousel>
+          <CarouselContent>
+            {groupItems(volunteerList, 6).map(
+              (group, index) => (
+                console.log('group :', group),
+                (
+                  <CarouselItem key={index} className="grid grid-cols-3 gap-6">
+                    {group.map((item, subIndex) => (
+                      <ExperienceBox
+                        key={subIndex}
+                        title={item.title}
+                        src={item.src}
+                      />
+                    ))}
+                  </CarouselItem>
+                )
+              ),
+            )}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </div>
   );
