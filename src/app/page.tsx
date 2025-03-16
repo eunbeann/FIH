1a8a9487d86e59b9939f7c5154219fd8fb8ca574
eunbeann <ds5bean23@gmail.com>
2025-03-16 11:04:58 -0700

    feat: add CareerPage component to main layout and adjust snap behavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CareerPage from './career/page';
 import ContactPage from './contact/page';
 import Home from './home/page';
 import ProjectPage from './projects/page';
@@ -7,19 +8,23 @@ export default function Main() {
   return (
     <>
       <div className="scrollbar-hidden flex h-[100vh-10rem] w-screen flex-col gap-y-[8rem] overflow-auto overflow-x-hidden xl:block xl:h-full xl:snap-y xl:snap-mandatory">
-        <div className="flex h-fit xl:h-screen xl:snap-start">
+        <div className="flex h-fit xl:h-screen">
           <Home />
         </div>
 
-        <div className="flex h-fit xl:h-screen xl:snap-start">
+        <div className="flex h-fit xl:h-screen">
+          <CareerPage />
+        </div>
+
+        <div className="flex h-fit xl:h-screen">
           <ProjectPage />
         </div>
 
-        <div className="flex h-fit xl:h-screen xl:snap-start">
+        <div className="flex h-fit xl:h-screen">
           <VolunteerPage />
         </div>
 
-        <div className="flex h-fit xl:h-screen xl:snap-start">
+        <div className="flex h-fit xl:h-screen">
           <ContactPage />
         </div>
       </div>
